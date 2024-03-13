49e94fe1991dafdd0aa2b1bc76d2d811abcbf4d8
Jakub Łysiuk <242457@edu.p.lodz.pl>
2024-03-14 00:46:02 +0100

    Attention bar (something's not right)
    NullReferenceException ;(

---
+++ Assets/Scripts/AttentionBar.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[ExecuteInEditMode]
+public class AttentionBar : MonoBehaviour
+{
+    
+    public int maximum;
+    public int current;
+    public Image mask;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        getCurrentFill();
+    }
+
+    public void updateAttention(int amount)
+    {
+        UnityEngine.Debug.Log("attention updated");
+        current += amount;
+    }
+
+    
+    public void getCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+
+}
--- Assets/Scripts/Enemies/EnemyLogic.cs
+++ Assets/Scripts/Enemies/EnemyLogic.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyLogic : MonoBehaviour
 {
     [SerializeField] float hp;
+    [SerializeField] GameObject attentionBar;
     public HumanBehavior behavior;
 
     public void dealDamage(float amount, Transform source)
     {
         hp = Mathf.Max(0, hp - amount);
         behavior.Alert(source);
-        if (hp == 0) Destroy(gameObject);
+        if (hp == 0)
+        {
+            attentionBar.GetComponent<AttentionBar>().updateAttention(10);
+            Destroy(gameObject);
+            
+        }
     }
 }
