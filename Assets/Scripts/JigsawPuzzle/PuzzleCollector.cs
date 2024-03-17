da3cdf2193a4fb9dffb49968c4c8f61129aafb60
Grzegorz Bednarek <gregorooooo02@gmail.com>
2024-03-17 21:04:36 +0100

    Added puzzle pieces prefab to the mission map; creating UI to show how many puzzles are left to collect

---
+++ Assets/Scripts/JigsawPuzzle/PuzzleCollector.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleCollector : MonoBehaviour
+{
+    private static PuzzleCollector instance;
+    public static int puzzleCount;
+    
+    private void Start()
+    {
+        puzzleCount = 0;
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    public static void AddPuzzle()
+    {
+        puzzleCount++;
+    }
+
+    public static void ResetPuzzle()
+    {
+        puzzleCount = 0;
+        
+    }
+}
