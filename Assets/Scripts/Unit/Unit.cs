cf7b86328e6636917428f346d2f7db19ded780c1
Jakub Łysiuk <242457@edu.p.lodz.pl>
2024-03-18 23:13:46 +0100

    basic health bars for units
    działa, póki co tylko dla naszych i jest po prostu dzieckiem jednostki

--- Assets/Scripts/Unit/Unit.cs
+++ Assets/Scripts/Unit/Unit.cs
@@ -2,17 +2,45 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
-    [SerializeField] double hp;
+    [SerializeField] double maxHp;
+    public double hp;
     [SerializeField] double armor = 1;
+
+    public GameObject healthBarUI;
+    public Slider healthBar;
 
     private void Start()
     {
-        UnitSelectionManager.Instance.allUnitsList.Add(gameObject);        
+        hp = maxHp;
+        UnitSelectionManager.Instance.allUnitsList.Add(gameObject);  
+        healthBarUI.SetActive(false);
+        healthBar.value = CalculateHealth();
+    }
+    
+    private void Update()
+    {
+        healthBar.value = CalculateHealth();
+
+        if (hp < maxHp)
+        {
+            healthBarUI.SetActive(true);
+        }
+
+        if (hp > maxHp)
+        {
+            hp = maxHp;
+        }
     }
 
+    float CalculateHealth()
+    {
+        return (float)(hp / maxHp);
+    }
+    
     private void OnDestroy()
     {
         UnitSelectionManager.Instance.allUnitsList.Remove(gameObject);
